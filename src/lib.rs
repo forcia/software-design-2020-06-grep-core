20a4c19346923374a06f0cb0d1060f0f62ec99c7
matsumoto <kentaro_matsumoto@forcia.com>
2020-03-30 18:52:44 +0900

    init commit

---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+use regex::Regex;
+
+
+pub trait MatcherTrait {
+    fn execute(&self, line: &str) -> bool;
+}
+
+pub struct ExtendedRegexpMatcher {
+    pattern: Regex,
+}
+impl ExtendedRegexpMatcher {
+    fn new(pattern: &str) -> ExtendedRegexpMatcher {
+        ExtendedRegexpMatcher {
+            pattern: Regex::new(pattern).unwrap(),
+        }
+    }
+}
+impl MatcherTrait for ExtendedRegexpMatcher {
+    fn execute(&self, line: &str) -> bool {
+        self.pattern.is_match(line)
+    }
+}
+
+pub struct FixedStringsMatcher<'a> {
+    pattern: &'a str,
+}
+impl<'a> FixedStringsMatcher<'a> {
+    fn new(pattern: &str) -> FixedStringsMatcher {
+        FixedStringsMatcher { pattern: pattern }
+    }
+}
+impl<'a> MatcherTrait for FixedStringsMatcher<'a> {
+    fn execute(&self, line: &str) -> bool {
+        line.contains(self.pattern)
+    }
+}
+
+pub enum Matcher<'a> {
+    ExtendedRegexp(ExtendedRegexpMatcher),
+    FixedStrings(FixedStringsMatcher<'a>),
+}
+
+pub struct GrepResult {
+    file_path: String,
+    hit_lines: Vec<String>,
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
